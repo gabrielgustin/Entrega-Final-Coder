f5ba7c2e18e19ae4dcb203211c7b58ffebd160aa
Gabriel Muñoz <85855452+gabrielgustin@users.noreply.github.com>
2022-12-08 19:54:11 -0300

    Add files via upload
    Firs Commit

---
+++ Proyecto Lulu Velas/views/Velas/stock.js
@@ -0,0 +1,10 @@
+let stockProductos = [
+    {id: 1, nombre: "Copito", tipo: "vela", cantidad: 1, desc: "Una hermosa vela aromatica", precio: 2000, img: './img/Copito.jpeg'},
+    {id: 2, nombre: "Copon", tipo: "vela", cantidad: 1, desc: "Una hermosa vela aromatica", precio: 4500, img: './img/Copon.jpeg'},
+    {id: 3, nombre: "Gladiator", tipo: "vela", cantidad: 1, desc: "Una hermosa vela aromatica", precio: 1500, img: './img/Gladiator.jpeg'},
+    {id: 4, nombre: "Indiana", tipo: "vela", cantidad: 1, desc: "Una hermosa vela aromatica", precio: 3000, img: './img/Indiana.jpeg'},
+    {id: 5, nombre: "Tom", tipo: "vela", cantidad: 1, desc: "Una hermosa vela aromatica", precio: 1500, img: './img/Tom.jpeg'},
+    {id: 6, nombre: "Valkiria", tipo: "vela", cantidad: 1, desc: "Una hermosa vela aromatica", precio: 3000, img: './img/Valkiria.jpeg'},
+    {id: 7, nombre: "Yackie", tipo: "vela", cantidad: 1, desc: "Una hermosa vela aromatica", precio: 5000, img: './img/Yackie.jpeg'},
+    {id: 8, nombre: "Titi", tipo: "vela", cantidad: 1, desc: "Una hermosa vela aromatica", precio: 1500, img: './img/Titi.jpeg'},
+]
